9f0a3b75e87536c466ae668474d9d48db0513024
unknown <jeckylike9@gmail.com>
2022-11-22 12:17:59 +0700

    2018115

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        final OneTimeWorkRequest request = new
+                OneTimeWorkRequest.Builder(MyWorker.class).build();
+        binding.button.setOnClickListener(new
+                                                  View.OnClickListener() {
+                                                      @Override
+                                                      public void onClick(View view) {
+                                                          WorkManager.getInstance().enqueueUniqueWork(
+                                                                  "Notifikasi", ExistingWorkPolicy.REPLACE,
+                                                                  request);
+                                                      }
+                                                  });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MyWorker.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull
+            WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+    @NonNull
+    @Override
+    public Result doWork() {
+        for (int i=1; i<=5; i++){
+            try{
+                Thread.sleep(1000);
+                displayNotification("pesan", " haloo "+i);
+            }catch (InterruptedException r){
+                r.getMessage();
+            }
+        }
+
+        return Result.success();
+    }
+    public void displayNotification(String task, String desc) {
+        NotificationManager manager =
+                (NotificationManager)
+
+                        getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new
+                NotificationChannel("pesan", "test123",
+                NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+        NotificationCompat.Builder builder = new
+
+                NotificationCompat.Builder(getApplicationContext(),
+                "pesan")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.drawable.ic_baseline_contactless_24)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        manager.notify(1, builder.build());
+    }
+}
